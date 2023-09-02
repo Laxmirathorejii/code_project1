5e64b97dece9c0b996b328630a27a4c4fd43a60c
laxmirathorejii <laxmirathore16122002@gmail.com>
2023-09-02 01:18:05 -0700

    first commit

---
+++ nodejs/appendfile.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+fs.appendFile("nodejs_architecture.txt",'Compared to other server-side languages, Node js has distinct advantages. Its asynchronous model and non-blocking I/O operation improve the scalability and performance of web applications built on other frameworks. Node js can easily handle multiple client requests without requiring multiple threads, consuming less memory and resources. Additionally, it is highly scalable and provides high performance. Node js is also flexible with multiple frameworks and makes the development process easier.',function(err){
+
+ if(err){
+    console.log('err')
+ }
+        
+else{
+
+console.log("succefully");
+}
+})
